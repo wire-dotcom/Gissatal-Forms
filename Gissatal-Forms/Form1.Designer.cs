5d98f443c02adb52ed00332836fe70f3644d1b0a
wire-dotcom <wire03001@utb.vaxjo.se>
2020-11-23 10:48:12 +0100

    Commit 1: Spelet fungerar som det ska, ett slumpmässigt tal mellan 1 och 100 väljs, spelaren får gissa och får ledtrådar beroende på om han/hon gissade över eller under det slumpmässiga talet, när spelaren gissar rätt så berättas det även hur många försök som behövdes.

--- Gissatal-Forms/Form1.Designer.cs
+++ Gissatal-Forms/Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace Gissatal_Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Svara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(309, 75);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(194, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Gissatal-Forms/Form1.cs
+++ Gissatal-Forms/Form1.cs
@@ -12,9 +12,48 @@ namespace Gissatal_Forms
 {
     public partial class Form1 : Form
     {
+        private int rndtal;
+        private int antalGuess;
         public Form1()
         {
             InitializeComponent();
+            Random rndint = new Random(); // Våran slumpmäsiga int variabel.            
+            rndtal = rndint.Next(1, 101); // Här sätts värdet på variablen till ett slumpmässigt tal mellan 0 och 100.
+            int antalGuess = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            antalGuess++;
+            string strsvar = textBox1.Text;
+            int intsvar = Convert.ToInt32(strsvar);
+            if (intsvar == rndtal)
+            {
+                richTextBox1.Text = ("Du har gissat rätt, bra jobbat!                    Det tog dig " + antalGuess + " försök");
+            }
+            if (intsvar > rndtal)
+            {
+                richTextBox1.Text = ("Fel! Du har gissat för högt! Försök igen");
+            }
+            if (intsvar < rndtal)
+            {
+                richTextBox1.Text = ("Fel! Du har gissat för lågt! Försök igen");
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
